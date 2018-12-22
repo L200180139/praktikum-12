ecf9089e8755fb5b7921d8aa9cab2793d0e0c54f
Muhammad Saiful Mujab <44234359+L200180139@users.noreply.github.com>
2018-12-22 23:43:54 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,48 @@
+print """<!DOCTYPE html>"""
+print
+print """<html>
+<pre>python -m SimpleHTTPServer</pre>
+<head><title>Tampilan web no 3</title></head>
+
+<table>
+	<tr>
+		<td><b><font size='3'>DATA DIRI</font></b></td>
+	</tr>
+	<tr>
+		<td>Nama</td>
+		<td>:</td>
+		<td>Muhammad Saiful Mujab</td>
+	</tr>
+	<tr>
+		<td>NIM</td>
+		<td>:</td>
+		<td>L200180139</td>
+	</tr>
+	<tr>
+		<td>Buku Favorit</td>
+		<td>:</td>
+		<td>5 menara</td>
+	</tr>
+	<tr>
+		<td>Idola di kalangan sahabat</td>
+		<td>:</td>
+		<td>Umar bin Khattab</td>
+	</tr>
+	<tr>
+		<td>Motto</td>
+		<td>:</td>
+		<td>lapar yo madang</td>
+	</tr>
+	<tr>
+		<td>Hobi</td>
+		<td>:</td>
+		<td>Olahraga</td>
+	</tr>
+	<tr>
+		<td>Status</td>
+		<td>:</td>
+		<td>Single</td>
+	</tr>
+</table>
+</body>
+</html>"""
